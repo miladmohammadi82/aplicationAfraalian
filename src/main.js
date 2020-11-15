72ba427dcb79df553d4f5925298d3aff035945f0
miladMohammadi <afraaliansite@gmail.com>
2020-11-15 12:26:23 -0800

    commplate commit in breach master

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 window.Vue = require('vue')
-import vuetify from './plugins/vuetify';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
 
 
 const app = new Vue({
