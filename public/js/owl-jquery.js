52556db6e7c82998181718a290a1a77979883ae5
miladMohammadi <afraaliansite@gmail.com>
2020-11-17 13:00:00 -0800

    ok

--- public/js/owl-jquery.js
+++ public/js/owl-jquery.js
@@ -13,19 +13,18 @@ $(document).ready(function () {
         loop: true,
         nav: true,
         rtl : true,
-        margin:10,
         dots: false,
+        autoplay:true,
+        autoplayTimeout:500,
         responsive: {
             0: {
-                items: 1
+                items: 1,
             },
-
             600: {
-                items: 3
+                items: 2
             },
-
             1000: {
-                items: 5
+                items: 4,
             }
         }
     })
---
+++ src/components/button.vue
@@ -0,0 +1,13 @@
+<template>
+  <md-button class="md-raised">Default</md-button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
