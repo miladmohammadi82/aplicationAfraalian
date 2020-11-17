34fc110ae8e888ddb9d999728a431130955e14cd
miladMohammadi <afraaliansite@gmail.com>
2020-11-17 00:22:28 -0800

    create slider product

--- public/js/owl-jquery.js
+++ public/js/owl-jquery.js
@@ -7,6 +7,27 @@ $(document).ready(function () {
         autoplay:true,
         autoplayTimeout:5000,
         nav:true,
-
     })
+
+    $(".owl-into").owlCarousel({
+        loop: true,
+        nav: true,
+        rtl : true,
+        margin:10,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            600: {
+                items: 3
+            },
+
+            1000: {
+                items: 5
+            }
+        }
+    })
+
 })
