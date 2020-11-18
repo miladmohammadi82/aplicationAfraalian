b90ee4c46ec9873091c9a9dc00b15804bf24006d
miladMohammadi <afraaliansite@gmail.com>
2020-11-17 22:15:48 -0800

    mearge shod branch mahsolatMa

--- public/js/owl-jquery.js
+++ public/js/owl-jquery.js
@@ -11,20 +11,22 @@ $(document).ready(function () {
 
     $(".owl-into").owlCarousel({
         loop: true,
-        nav: true,
         rtl : true,
-        dots: false,
+        dots: true,
+        nav: true,
         autoplay:true,
+        responsiveClass:true,
         autoplayTimeout:500,
         responsive: {
             0: {
                 items: 1,
             },
             600: {
-                items: 2
+                items: 3
             },
             1000: {
                 items: 4,
+                nav: true,
             }
         }
     })
